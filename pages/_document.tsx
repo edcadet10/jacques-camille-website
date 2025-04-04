000f22775f257d2a349c61efe1ff4b8f0b073aaa
YourGitHubUsername <your.email@example.com>
2025-04-04 18:29:05 -0400

    Fix: Configure Netlify CMS with proper Identity integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,21 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Netlify Identity Widget */}
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            if (window.netlifyIdentity) {
+              window.netlifyIdentity.on("init", user => {
+                if (!user) {
+                  window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                  });
+                }
+              });
+            }
+          `
+        }} />
       </body>
     </Html>
   );
